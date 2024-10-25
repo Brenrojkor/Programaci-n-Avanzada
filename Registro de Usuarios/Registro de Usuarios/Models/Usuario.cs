5d0882a8b15d91e5691e4da4295664c035967d72
Brenda Rojas <145055735+Brenrojkor@users.noreply.github.com>
2024-10-24 21:29:51 -0600

    nuevoproyecto

---
+++ Registro de Usuarios/Registro de Usuarios/Models/Usuario.cs
@@ -0,0 +1,9 @@
+﻿namespace Registro_de_Usuarios.Models
+{
+    public class Usuario
+    {
+        public int idUsuario { get; set; }
+        public string usuario { get; set; }
+        public string password { get; set; }
+    }
+}
