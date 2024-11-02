6c0f673affbd54c7acb9bcb3b69ec2cf21252264
Brenda Rojas <145055735+Brenrojkor@users.noreply.github.com>
2024-11-01 20:02:16 -0600

    Roles y Usuario

---
+++ Proyecto_G2.sql
@@ -0,0 +1,17 @@
+-- Proyecto Progra Avanzada G2
+USE u484426513_pac324;
+
+CREATE TABLE RolG2P (
+RolID INT NOT NULL PRIMARY KEY,
+NombreRol VARCHAR(50) NOT NULL
+);
+
+
+CREATE TABLE UsuariosG2P (
+	UsuarioID INT NOT NULL PRIMARY KEY,
+    NombreUsuario VARCHAR(45) UNIQUE,
+	Contraseña  VARCHAR(80) NOT NULL,
+    NombreCompleto VARCHAR(100),
+    RolID INT, FOREIGN KEY (RolID) REFERENCES RolG2P(RolID)
+);
+
