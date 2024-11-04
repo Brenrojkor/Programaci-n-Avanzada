c8f52019d54d85af872cb7f6914d35ac355332e9
Cristopher Rodríguez <145072878+EnekoRAP@users.noreply.github.com>
2024-11-04 14:40:55 -0600

    Creación del MVC del proyecto

---
+++ ProyectoG2_Pokedex/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProyectoG2_Pokedex.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
