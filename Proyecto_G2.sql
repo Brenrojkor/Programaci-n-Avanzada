45c9ab7b5833104a88342b6d760f153c088a1c34
Cristopher Rodríguez <145072878+EnekoRAP@users.noreply.github.com>
2024-11-17 18:36:34 -0600

    Update Proyecto_G2.sql

--- Proyecto_G2.sql
+++ Proyecto_G2.sql
@@ -31,19 +31,59 @@ CREATE TABLE Pokedex (
 >>>>>>> Brenda
 
 CREATE TABLE EntrenadoresPokemons (
-EntrnadorPokemonID INT PRIMARY KEY,
-UsuarioID INT,
-PokedexID INT,
-Nivel INT,
-Estado VARCHAR(50),
-FOREIGN KEY (UsuarioID) REFERENCES UsuariosG2P(UsuarioID),
-FOREIGN KEY (PokedexID) REFERENCES Pokedex(PokedexID)
+	EntrnadorPokemonID INT PRIMARY KEY,
+	UsuarioID INT,
+	PokedexID INT,
+	Nivel INT,
+	Estado VARCHAR(50),
+	FOREIGN KEY (UsuarioID) REFERENCES UsuariosG2P(UsuarioID),
+	FOREIGN KEY (PokedexID) REFERENCES Pokedex(PokedexID)
 );
 
 CREATE TABLE EquipoLucha (
-EquipoID INT PRIMARY KEY,
-UsuarioID INT,
-EntrenadorPokemonID INT,
-FOREIGN KEY (UsuarioID) REFERENCES UsuariosG2P(UsuarioID),
-FOREIGN KEY (EntrenadorPokemonID) REFERENCES EntrenadoresPokemons(EntrenadorPokemonID)
+	EquipoID INT PRIMARY KEY,
+	UsuarioID INT,
+	EntrenadorPokemonID INT,
+	FOREIGN KEY (UsuarioID) REFERENCES UsuariosG2P(UsuarioID),
+	FOREIGN KEY (EntrenadorPokemonID) REFERENCES EntrenadoresPokemons(EntrenadorPokemonID)
+);
+
+>>>>>>> Paola
+
+CREATE TABLE G2_Retos (
+    RetoID INT PRIMARY KEY,
+    EntrenadorRetadorID INT,
+    EntrenadorRetadoID INT,
+    EstadoReto VARCHAR2(10) CHECK (EstadoReto IN ('Pendiente', 'Aceptado', 'Rechazado', 'Finalizado')),
+    FechaReto DATE DEFAULT SYSDATE,
+    FechaFinalizacion DATE,
+    CONSTRAINT fk_entrenador_retador FOREIGN KEY (EntrenadorRetadorID) REFERENCES Usuarios(UsuarioID),
+    CONSTRAINT fk_entrenador_retado FOREIGN KEY (EntrenadorRetadoID) REFERENCES Usuarios(UsuarioID)
+);
+
+CREATE TABLE G2_Mensajes (
+    MensajeID INT PRIMARY KEY,
+    UsuarioOrigenID INT,
+    UsuarioDestinoID INT,
+    Contenido VARCHAR(1000),
+    FechaEnvio DATE DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_usuario_origen FOREIGN KEY (UsuarioOrigenID) REFERENCES Usuarios(UsuarioID),
+    CONSTRAINT fk_usuario_destino FOREIGN KEY (UsuarioDestinoID) REFERENCES Usuarios(UsuarioID)
+);
+
+>>>>>>> Jose
+
+CREATE TABLE G2_AtencionEnfermeria (
+	AtencionID INT PRIMARY KEY,
+    EntrnadorPokemonID INT
+    FechaSolicitud DATE
+    FOREIGN KEY (EntrenadorPokemonID) REFERENCES EntrenadoresPokemons(EntrenadorPokemonID)
+);
+
+CREATE TABLE G2_HistorialAtenciones (
+	HistorialID INT PRIMARY KEY,
+    AtencionID INT,
+    FOREIGN KEY (AtencionID) REFERENCES AtencionEnfermeria(AntencionID),
+    FechaAtencion DATE,
+    DescripcionAtencion VARCHAR(1000)
 );
