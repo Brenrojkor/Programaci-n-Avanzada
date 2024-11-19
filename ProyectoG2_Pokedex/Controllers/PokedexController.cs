832735f406cbf40129baeb098f41ac468e29cce9
Brenda Rojas <145055735+Brenrojkor@users.noreply.github.com>
2024-11-18 23:52:56 -0600

    Pokedex api

---
+++ ProyectoG2_Pokedex/Controllers/PokedexController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoG2_Pokedex.Controllers
+{
+    public class PokedexController : Controller
+    {
+        public IActionResult Pokedex()
+        {
+            return View();
+        }
+    }
+}
