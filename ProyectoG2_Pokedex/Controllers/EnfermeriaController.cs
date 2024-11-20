354be2bcc96be94552fc3030fa29158714827b33
Cristopher Rodríguez <145072878+EnekoRAP@users.noreply.github.com>
2024-11-20 14:32:40 -0600

    Update EnfermeriaController.cs

--- ProyectoG2_Pokedex/Controllers/EnfermeriaController.cs
+++ ProyectoG2_Pokedex/Controllers/EnfermeriaController.cs
@@ -4,7 +4,7 @@ namespace ProyectoG2_Pokedex.Controllers
 {
     public class EnfermeriaController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Enfermeria()
         {
             return View();
         }
