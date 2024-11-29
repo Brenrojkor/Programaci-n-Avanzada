58c18a5b23287610fb9a8f023bd5290606670337
Brenda Rojas <145055735+Brenrojkor@users.noreply.github.com>
2024-11-29 00:41:15 -0600

    login sin funcionar

---
+++ ProyectoG2_Pokedex/Controllers/LoginController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProyectoG2_Pokedex.Models;
+
+namespace ProyectoG2_Pokedex.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View(model);
+        }
+    }
+}
