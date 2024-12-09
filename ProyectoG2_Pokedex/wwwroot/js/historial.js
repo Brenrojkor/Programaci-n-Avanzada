217ec90b6befecbbfe30649da576e162c1e06af9
Cristopher Rodríguez <145072878+EnekoRAP@users.noreply.github.com>
2024-12-09 17:51:49 -0600

    Update historial.js

--- ProyectoG2_Pokedex/wwwroot/js/historial.js
+++ ProyectoG2_Pokedex/wwwroot/js/historial.js
@@ -1,18 +1,1 @@
-﻿document.addEventListener("DOMContentLoaded", () => {
-    const params = new URLSearchParams(window.location.search);
-    const name = params.get("name");
-    const trainer = params.get("trainer");
-    const status = params.get("status");
-
-    if (name && trainer && status) {
-        const historialContainer = document.getElementById("historial-container");
-        const entry = document.createElement("div");
-        entry.classList.add("historial-entry");
-        entry.innerHTML = `
-                <p><strong>Pokémon:</strong> ${name}</p>
-                <p><strong>Entrenador:</strong> ${trainer}</p>
-                <p><strong>Estado:</strong> ${status}</p>
-            `;
-        historialContainer.appendChild(entry);
-    }
-});
+﻿
