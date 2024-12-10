9a681d5faf0cb266dfb36c663d6070f28c534b38
Cristopher Rodríguez <145072878+EnekoRAP@users.noreply.github.com>
2024-12-09 22:27:33 -0600

    Update Proyecto_G2.sql

--- Proyecto_G2.sql
+++ Proyecto_G2.sql
@@ -1,23 +1,22 @@
 -- Proyecto Progra Avanzada G2
 USE u484426513_pac324;
 
-<<<<<<< HEAD
-=======
-CREATE TABLE RolG2P (
+/* HEAD */
+
+CREATE TABLE G2_Roles (
 	RolID INT NOT NULL PRIMARY KEY,
 	NombreRol VARCHAR(50) NOT NULL
 );
 
-
-CREATE TABLE UsuariosG2P (
+CREATE TABLE G2_Usuario (
 	UsuarioID INT NOT NULL PRIMARY KEY,
     NombreUsuario VARCHAR(45) UNIQUE,
 	Contraseña  VARCHAR(80) NOT NULL,
     NombreCompleto VARCHAR(100),
-    RolID INT, FOREIGN KEY (RolID) REFERENCES RolG2P(RolID)
+    RolID INT, FOREIGN KEY (RolID) REFERENCES G2_Roles(RolID)
 );
 
-CREATE TABLE Pokedex (
+CREATE TABLE G2_Pokedex (
     PokedexID INT PRIMARY KEY,
     NombrePokemon VARCHAR(50) NOT NULL,
     Tipo VARCHAR(50),
@@ -25,40 +24,40 @@ CREATE TABLE Pokedex (
     EvolucionID INT,
     Peso DECIMAL(5,2),
     Numero INT,
-    FOREIGN KEY (EvolucionID) REFERENCES Pokedex(PokedexID)
+    FOREIGN KEY (EvolucionID) REFERENCES G2_Pokedex(PokedexID)
 );
 
->>>>>>> Brenda
+/* Brenda */
 
-CREATE TABLE EntrenadoresPokemons (
+CREATE TABLE G2_Entrenadores (
 	EntrnadorPokemonID INT PRIMARY KEY,
 	UsuarioID INT,
 	PokedexID INT,
 	Nivel INT,
 	Estado VARCHAR(50),
-	FOREIGN KEY (UsuarioID) REFERENCES UsuariosG2P(UsuarioID),
-	FOREIGN KEY (PokedexID) REFERENCES Pokedex(PokedexID)
+	FOREIGN KEY (UsuarioID) REFERENCES G2_Usuario(UsuarioID),
+	FOREIGN KEY (PokedexID) REFERENCES G2_Pokedex(PokedexID)
 );
 
-CREATE TABLE EquipoLucha (
+CREATE TABLE G2_Equipos (
 	EquipoID INT PRIMARY KEY,
 	UsuarioID INT,
 	EntrenadorPokemonID INT,
-	FOREIGN KEY (UsuarioID) REFERENCES UsuariosG2P(UsuarioID),
-	FOREIGN KEY (EntrenadorPokemonID) REFERENCES EntrenadoresPokemons(EntrenadorPokemonID)
+	FOREIGN KEY (UsuarioID) REFERENCES G2_Usuario(UsuarioID),
+	FOREIGN KEY (EntrenadorPokemonID) REFERENCES G2_Entrenadores(EntrenadorPokemonID)
 );
 
->>>>>>> Paola
+/* Paola */
 
 CREATE TABLE G2_Retos (
     RetoID INT PRIMARY KEY,
     EntrenadorRetadorID INT,
     EntrenadorRetadoID INT,
-    EstadoReto VARCHAR2(10) CHECK (EstadoReto IN ('Pendiente', 'Aceptado', 'Rechazado', 'Finalizado')),
-    FechaReto DATE DEFAULT SYSDATE,
+    EstadoReto VARCHAR(10) CHECK (EstadoReto IN ('Pendiente', 'Aceptado', 'Rechazado', 'Finalizado')),
+    FechaReto DATE,
     FechaFinalizacion DATE,
-    CONSTRAINT fk_entrenador_retador FOREIGN KEY (EntrenadorRetadorID) REFERENCES Usuarios(UsuarioID),
-    CONSTRAINT fk_entrenador_retado FOREIGN KEY (EntrenadorRetadoID) REFERENCES Usuarios(UsuarioID)
+    CONSTRAINT fk_entrenador_retador FOREIGN KEY (EntrenadorRetadorID) REFERENCES G2_Usuario(UsuarioID),
+    CONSTRAINT fk_entrenador_retado FOREIGN KEY (EntrenadorRetadoID) REFERENCES G2_Usuario(UsuarioID)
 );
 
 CREATE TABLE G2_Mensajes (
@@ -67,23 +66,25 @@ CREATE TABLE G2_Mensajes (
     UsuarioDestinoID INT,
     Contenido VARCHAR(1000),
     FechaEnvio DATE DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT fk_usuario_origen FOREIGN KEY (UsuarioOrigenID) REFERENCES Usuarios(UsuarioID),
-    CONSTRAINT fk_usuario_destino FOREIGN KEY (UsuarioDestinoID) REFERENCES Usuarios(UsuarioID)
+    CONSTRAINT fk_usuario_origen FOREIGN KEY (UsuarioOrigenID) REFERENCES G2_Usuario(UsuarioID),
+    CONSTRAINT fk_usuario_destino FOREIGN KEY (UsuarioDestinoID) REFERENCES G2_Usuario(UsuarioID)
 );
 
->>>>>>> Jose
+/* Jose */
 
-CREATE TABLE G2_AtencionEnfermeria (
+CREATE TABLE G2_Enfermeria (
 	AtencionID INT PRIMARY KEY,
-    EntrnadorPokemonID INT
-    FechaSolicitud DATE
-    FOREIGN KEY (EntrenadorPokemonID) REFERENCES EntrenadoresPokemons(EntrenadorPokemonID)
+    EntrnadorPokemonID INT,
+    FechaSolicitud DATE,
+    FOREIGN KEY (EntrenadorPokemonID) REFERENCES G2_Entrenadores(EntrenadorPokemonID)
 );
 
-CREATE TABLE G2_HistorialAtenciones (
+CREATE TABLE G2_Historial (
 	HistorialID INT PRIMARY KEY,
     AtencionID INT,
-    FOREIGN KEY (AtencionID) REFERENCES AtencionEnfermeria(AntencionID),
+    FOREIGN KEY (AtencionID) REFERENCES G2_Enfermeria(AntencionID),
     FechaAtencion DATE,
     DescripcionAtencion VARCHAR(1000)
 );
+
+/* Cristopher */
