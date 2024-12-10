0eeb781595b5e4104a4144d1760c61af1c80a146
Cristopher Rodríguez <145072878+EnekoRAP@users.noreply.github.com>
2024-12-09 22:51:04 -0600

    Create PokedexDBContext.cs

---
+++ ProyectoG2_Pokedex/Data/PokedexDBContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoG2_Pokedex.Data
+{
+    public class PokedexDBContext : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
