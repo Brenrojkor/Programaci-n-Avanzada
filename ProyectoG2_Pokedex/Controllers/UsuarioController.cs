679b3dadf451e03297d4c08a466dfb424b418abc
Cristopher Rodríguez <145072878+EnekoRAP@users.noreply.github.com>
2024-12-11 22:02:19 -0600

    Config de Usuarios

---
+++ ProyectoG2_Pokedex/Controllers/UsuarioController.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProyectoG2_Pokedex.Data;
+using ProyectoG2_Pokedex.Models;
+
+namespace ProyectoG2_Pokedex.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        private readonly MinombredeconexionDbContext _context;
+
+        public UsuariosController(MinombredeconexionDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Usuarios()
+        {
+            var usuarios = _context.Usuarios.ToList();
+            return View(usuarios);
+        }
+
+        public IActionResult Agregar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Agregar(UsuariosModel usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(usuario);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Usuarios));
+            }
+            return View(usuario);
+        }
+
+        public IActionResult Editar(int? id)
+        {
+            if (id == null || _context.Usuarios == null)
+            {
+                return NotFound();
+            }
+
+            var usuario = _context.Usuarios.Find(id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+            return View(usuario);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Editar(int id, UsuariosModel usuario)
+        {
+            if (id != usuario.IdUsuario)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(usuario);
+                    _context.SaveChanges();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!_context.Usuarios.Any(e => e.IdUsuario == id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Usuarios));
+            }
+            return View(usuario);
+        }
+
+        public IActionResult Eliminar(int? id)
+        {
+            if (id == null || _context.Usuarios == null)
+            {
+                return NotFound();
+            }
+
+            var usuario = _context.Usuarios
+                .FirstOrDefault(m => m.IdUsuario == id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            return View(usuario);
+        }
+
+        [HttpPost, ActionName("Eliminar")]
+        [ValidateAntiForgeryToken]
+        public IActionResult Confirmar(int id)
+        {
+            var usuario = _context.Usuarios.Find(id);
+            if (usuario != null)
+            {
+                _context.Usuarios.Remove(usuario);
+                _context.SaveChanges();
+            }
+            return RedirectToAction(nameof(Usuarios));
+        }
+    }
+}
