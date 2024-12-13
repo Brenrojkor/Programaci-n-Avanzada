e812f98911ab5b86a4ca718b72b143e76e9a88bb
Brenda Rojas <145055735+Brenrojkor@users.noreply.github.com>
2024-12-13 15:46:21 -0600

    ULTIMOS CAMBIOS

---
+++ ProyectoG2_Pokedex/Controllers/EquipoLuchaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoG2_Pokedex.Controllers
+{
+    public class EquipoLuchaController : Controller
+    {
+        public IActionResult EquipoLucha()
+        {
+            return View();
+        }
+    }
+}
